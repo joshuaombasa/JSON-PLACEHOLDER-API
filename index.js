88a29468b99457b66385555ed1d0f4aea98cf825
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-05 22:29:07 +0300

    added script

---
+++ index.js
@@ -0,0 +1,15 @@
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+
+       const postsHtml = postsArr.map((post) => {
+           return `
+                  <h4>${post.title}</h4>
+                  <p>${post.body}</p>
+                  <hr>
+                  `
+       }).join("")
+       document.getElementById("container").innerHTML = postsHtml
+       console.log(postsHtml)
+    })
