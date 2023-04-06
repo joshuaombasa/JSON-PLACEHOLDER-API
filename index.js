f1a695d50fc74669c67abdf0f9e1f4604fb6ff1d
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-06 17:08:03 +0300

    updated script

--- index.js
+++ index.js
@@ -4,20 +4,22 @@ const inputContent = document.getElementById("input-content")
 
 let postsArr = []
 
+function renderPosts() {
+    const postsHtml = postsArr.map((post) => {
+        return `
+               <h4>${post.title}</h4>
+               <p>${post.body}</p>
+               <hr>
+               `
+    }).join("")
+    document.getElementById("container").innerHTML = postsHtml
+}
+
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
         postsArr = data.slice(0, 5)
-
-       const postsHtml = postsArr.map((post) => {
-           return `
-                  <h4>${post.title}</h4>
-                  <p>${post.body}</p>
-                  <hr>
-                  `
-       }).join("")
-       document.getElementById("container").innerHTML = postsHtml
        
     })
 
